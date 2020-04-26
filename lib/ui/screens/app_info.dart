5907bb4e5e01ccc44cbc900d3920952b0f4aa503
lokesh-coder <mexican.dirtyfellow@gmail.com>
2020-04-26 11:42:25 +0530

    add app info

--- lib/ui/screens/app_info.dart
+++ lib/ui/screens/app_info.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:monex/config/colors.dart';
+import 'package:package_info/package_info.dart';
 import 'package:monex/config/labels.dart';
 import 'package:monex/ui/common/app-shell.dart';
 import 'package:monex/ui/common/header.dart';
 
-class AppInfoScreen extends StatelessWidget {
+class AppInfoScreen extends StatefulWidget {
   const AppInfoScreen({Key key}) : super(key: key);
+
+  @override
+  _AppInfoScreenState createState() => _AppInfoScreenState();
+}
+
+class _AppInfoScreenState extends State<AppInfoScreen> {
+  PackageInfo info;
+
+  @override
+  void initState() {
+    super.initState();
+    PackageInfo.fromPlatform().then((PackageInfo packageInfo) {
+      setState(() {
+        info = packageInfo;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +39,36 @@ class AppInfoScreen extends StatelessWidget {
         leading: Container(),
       ),
       child: Container(
-        child: Text('im info'),
+        child: ListView(
+          children: _getItems(info),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _getItems(PackageInfo info) {
+    if (info == null) return [];
+    List<Map> items = [
+      {'Name': info.appName},
+      {'Version': info.version},
+      {'Build number': info.buildNumber},
+      {'Package name': info.packageName},
+    ];
+    List<Widget> tiles = [];
+    items.forEach((item) {
+      tiles.add(_getItem(item.keys.first, item.values.first));
+    });
+    return tiles;
+  }
+
+  _getItem(String title, String value) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(bottom: BorderSide(color: Clrs.dark.withOpacity(0.1))),
+      ),
+      child: ListTile(
+        title: Text(title),
+        subtitle: Text(value),
       ),
     );
   }
